732960e71e803ddca2dd083b8c94067e9ed9eaae
DankeMary <laktionofs@mail.ru>
2017-05-03 14:38:10 +0300

    Added functions

---
+++ pharmacy/HelpUtils.cpp
@@ -0,0 +1,47 @@
+#include "stdafx.h"
+#include "HelpUtils.h"
+#include "Med.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+string skipFieldsNames(istream &is)
+{
+	string str;
+	is >> str;
+	while ((str.find(":") != -1) || str == "\t" || str == "\n" || str == " ")
+	{
+		is >> str;
+	}
+	return str;
+}
+
+string boolAsString(bool b)
+{
+	return b ? "+": "-";
+}
+
+bool boolFromString(string str)
+{
+	if (str.find("+") != -1) 
+		return true;
+	else return false;
+}
+
+Date dateFromString(string str)
+{
+	Date date = Date();
+	int i = 0;
+	while (str[i] == ' ' || i < str.length())
+	{
+		i++;
+	}
+	date.day = stod(str.substr(i, 2));
+	i += 3;
+	date.month = stod(str.substr(i, 2));
+	i += 3;
+	date.year = stod(str.substr(i, 4));
+	return date;
+}
---
+++ pharmacy/HelpUtils.h
@@ -0,0 +1,7 @@
+#pragma once
+
+
+string skipFieldsNames(istream &is);
+string boolAsString(bool b);
+bool boolFromString(string str);
+Date dateFromString(string str);
