0ce6d5fe34c88f5dc91019b8ea00e0dd7d8d7f9f
DankeMary <laktionofs@mail.ru>
2017-05-03 17:09:45 +0300

    Added NameComparator

---
+++ pharmacy/Comparators.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Comparators.h"
+
+
+Comparators::Comparators()
+{
+}
+
+
+Comparators::~Comparators()
+{
+}
--- pharmacy/Comparators.h
+++ pharmacy/Comparators.h
@@ -1,10 +1,20 @@
 #pragma once
 #include "Med.h"
+
 class FarmNumComparator
 {
-public:
-	bool operator() (Med m1, Med m2)
-	{
-		return m1.farmNum < m2.farmNum;
-	}
+	public:
+		bool operator() (Med m1, Med m2)
+		{
+			return m1.farmNum < m2.farmNum;
+		}
+};
+
+class NameComparator
+{
+	public:
+		bool operator() (Med m1, Med m2)
+		{
+			return m1.name < m2.name;
+		}
 };
