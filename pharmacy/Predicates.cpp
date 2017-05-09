46d1ae07be61f9d86d499047e3edb04c7050e981
DankeMary <laktionofs@mail.ru>
2017-05-09 19:17:10 +0300

    Added predicate for name

--- pharmacy/Predicates.cpp
+++ pharmacy/Predicates.cpp
@@ -1,6 +1,10 @@
 #include "stdafx.h"
 #include "Predicates.h"
+//#include <string>
 
+//using namespace std;
+
+//FarmNumPredicate
 FarmNumPredicate::FarmNumPredicate() {}
 FarmNumPredicate::FarmNumPredicate(int aFarmNum)
 {
@@ -13,4 +17,19 @@ bool FarmNumPredicate::operator() (Med med) {
 
 bool FarmNumPredicate::operator() (Med med1, Med med2) {
 	return med1.farmNum == med2.farmNum;
+}
+
+//NamePredicate
+NamePredicate::NamePredicate() {}
+NamePredicate::NamePredicate(std::string aName)
+{
+	fName = aName;
+}
+
+bool NamePredicate::operator()(Med med) {
+	return fName == med.name;
+}
+
+bool NamePredicate::operator()(Med med1, Med med2) {
+	return med1.name == med2.name;
 }
--- pharmacy/Predicates.h
+++ pharmacy/Predicates.h
@@ -1,15 +1,27 @@
 #pragma once
 #include "Med.h"
+#include <string>
 
+using namespace std;
 
 class FarmNumPredicate{
-protected:
-	int fFarmNum;
+	protected:
+		int fFarmNum;
 
-public:
-	FarmNumPredicate();
-	FarmNumPredicate(int aFarmNum);
-	bool operator() (Med med);
-	bool operator() (Med med1, Med med2);
+	public:
+		FarmNumPredicate();
+		FarmNumPredicate(int aFarmNum);
+		bool operator() (Med med);
+		bool operator() (Med med1, Med med2);
+};
+
+class NamePredicate {
+	protected: //????
+		std::string fName;
+	public:
+		NamePredicate();
+		NamePredicate(std::string aName);
+		bool operator() (Med med);
+		bool operator() (Med med1, Med med2);
 };
 
