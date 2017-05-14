88b6a8de66cb370cf5f23534a8dd48cb4127513a
DankeMary <laktionofs@mail.ru>
2017-05-14 15:31:42 +0300

    Added code to search line of main menu

--- pharmacy/pharmacy.cpp
+++ pharmacy/pharmacy.cpp
@@ -2,23 +2,29 @@
 //
 
 #include "stdafx.h"
-#include "stdafx.h"
 #include "PharmacyDataBase.h"
 #include "HelpUtils.h"
 #include "MenuUtils.h"
 #include <iostream>
+#include <string>
+#include "Med.h"
+
 
 int main()
 {
 	int option = 0;
 	PharmacyDataBase<> dataBase = PharmacyDataBase<>();
 	Med med;
-	string fileName;
-	bool inputFile;
+	string fileName, str;
+	Date date;
+	bool inputFile, simpleSearch, found;
+	vector<Med>::iterator it;
+
 	while (option != 0)
 	{
 		mainMenu();
 		option = getInt(option, 0, 5);
+		found = false;
 		switch (option)
 		{
 		case 1://Загрузить список
@@ -58,17 +64,73 @@ int main()
 		case 3://Поиск
 			searchTypeMenu();
 			option = getInt(option, 0, 2);
+
 			switch (option) 
 			{
 				case 1://Простой
-				
+					simpleSearch = true;
 					break;
 				case 2://Бинарный
-				
+					simpleSearch = false;
 					break;
 				case 0:
 					break;
 			}
+			findMenu();
+			option = getInt(option, 0, 3);
+			switch (option)
+			{
+				case 1://По номеру аптеки
+					cout << "Введите номер аптеки"<< endl;
+					if (simpleSearch)
+						found = dataBase.searchFarmNum(getInt(0), it);
+					else
+						found = dataBase.binarySearchFarmNum(getInt(0), it);
+					break;
+				case 2://По названию лекарства
+					cout << "Введите название лекарства" << endl;
+					if (simpleSearch)
+						found = dataBase.searchName(getString(), it);
+					else
+						found = dataBase.binarySearchName(getString(), it);
+					break;
+				case 3: //По дате поступления
+					cout << "Введите дату поступления" << endl;
+					getline(cin, str);
+					date = dateFromString(str);
+					if (simpleSearch)
+						found = dataBase.searchArrival(date, it);
+					else
+						found = dataBase.binarySearchArrival(date, it);
+					break;
+				case 0:
+					break;
+			}
+			if (found) {
+				cout << "Запись найдена\n";
+				actionsMenu();
+				option = getInt(option, 0, 3);
+				while (option != 0) 
+				{
+					switch (option) {
+					case 1:
+						cout << *it;
+						break;
+					case 2:
+						dataBase.changeItem(it);
+						break;
+					case 3:
+						dataBase.removeItem(it);
+						break;
+					case 0:
+						break;
+					}
+					actionsMenu();
+					option = getInt(option, 0, 3);
+				}
+			}
+			else if (!found &&  option != 0)
+				cout << "Запись не найдена!\n";
 			break;
 		case 4: //Добавить
 			med = getMed();
